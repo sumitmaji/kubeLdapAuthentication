6170ec41ed58021c45ffc3b6ff841b9bad17787c
Sumit Maji <skmaji1@outlook.com>
2018-06-25 08:09:12 +0530

    Check

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 // app.use(passport.initialize());
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
   console.log('Received')
   res.send('Hi')
 })
