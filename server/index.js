b550956f8fc513b863cf64c2dc5f1bd7e6037905
Sumit Maji <skmaji1@outlook.com>
2018-06-26 07:11:44 +0530

    Commit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var express      = require('express'),
 
 var OPTS = {
   server: {
-    url: 'ldap://localhost:389',
+    url: process.env.LDAP_HOST,
     bindDN: 'cn=admin,dc=default,dc=svc,dc=cloud,dc=uat',
     bindCredentials: process.env.LDAP_PASSWORD,
     searchBase: 'ou=users,dc=default,dc=svc,dc=cloud,dc=uat',
@@ -15,11 +15,11 @@ var OPTS = {
 
 var app = express();
 
-// passport.use(new LdapStrategy(OPTS));
+passport.use(new LdapStrategy(OPTS));
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(passport.initialize());
 
 app.get('/ldapauth/test', (req, res) => {
   res.send('Hi')
@@ -29,7 +29,7 @@ app.get('/healthz', (req, res) => {
   res.send('Ok')
 })
 
-app.post('/login', passport.authenticate('ldapauth', {session: false}), function(req, res) {
+app.post('/ldapauth/login', passport.authenticate('ldapauth', {session: false}), function(req, res) {
   res.send({status: 'ok'});
 });
 
